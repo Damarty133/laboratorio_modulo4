5049baf3930dbc073155965ce0a7f3d2112b2270
David Martinez <davidtennistrainer@yahoo.es>
2024-02-15 13:08:49 +0100

    Primer commit

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import "./style.css";
+
+console.log("vamos a calcular cuanto tiene que pagar cada comensal teniendo en cuenta que yo invito a las bebidas");
+console.log((120 - 18)/6+" Euros tendría que pagar cada comensal")  ;
