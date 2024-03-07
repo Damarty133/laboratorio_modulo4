e20bb90b2e000f17fbafda6d969a8b1d30e39f1a
David Martinez <davidtennistrainer@yahoo.es>
2024-03-07 13:33:23 +0100

    metiendo botoncambiarTurno dentro de la funcion eventos

--- src/main.ts
+++ src/main.ts
@@ -66,8 +66,9 @@ const cambiarTurno = () => {
     }
 }
 const nuevoEvento = () =>{const botonCambiarTurno = document.getElementById("botonCambiarTurno");
-        if (botonCambiarTurno !==null && botonCambiarTurno !== undefined && botonCambiarTurno instanceof HTMLButtonElement){
-            botonCambiarTurno.addEventListener("click", cambiarTurno);
+    if (botonCambiarTurno !==null && botonCambiarTurno !== undefined && botonCambiarTurno instanceof HTMLButtonElement){
+          botonCambiarTurno.addEventListener("click", cambiarTurno);
+    
     }
 }
     
@@ -75,89 +76,7 @@ const nuevoEvento = () =>{const botonCambiarTurno = document.getElementById("bot
 document.addEventListener("DOMContentLoaded",() => {
     pintarTurno();
     eventos();
-    nuevoEvento();
 });
 
 
-// let nuevoTurno = 0;
 
-// const pintarNuevoTurno = () => {
-//     const inputNuevoTurno = document.getElementById("numeroTurno");
-//   if (
-//     inputNuevoTurno !== undefined &&
-//     inputNuevoTurno !== null &&
-//     inputNuevoTurno instanceof HTMLInputElement
-//   ) {
-//     inputNuevoTurno.value = turno.toString().padStart(2, "0");
-//   } else {
-//     console.error("no se encuentra el input");
-//   }
-// };
-
-// const turnoNuevo = () => {
-//     turno = nuevoTurno;
-//     pintarNuevoTurno();
-
-// };
-
-// const nuevoEventos = () =>{
-//     const botonCambiarTurno = document.getElementById("botonMas");
-//     if( botonCambiarTurno !== null && botonCambiarTurno !== undefined && botonCambiarTurno instanceof HTMLButtonElement){
-//             botonCambiarTurno.addEventListener ("click", turnoNuevo)
-//     }
-//     else{
-//         console.error("error del boton cambiar turo");
-//         }
-//     }
-
-
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     pintarNuevoTurno();
-//     nuevoEventos();
-// })
-
-// const inputNumeroTurno = document.getElementById ("numeroTurno");
-// const botonMas = document.getElementById("botonMas");
-// const botonMenos = document.getElementById ("botonMenos");
-// const botonReset = document.getElementById ("botonReset");
-
-// if(inputNumeroTurno !== undefined && inputNumeroTurno !== null && botonMas !== null && botonMas !== undefined){
-//     botonMas.addEventListener ("click", ()=>{
-//     if(inputNumeroTurno instanceof HTMLInputElement){
-//         let numeroActual = Number(inputNumeroTurno.value) || 0;
-//     inputNumeroTurno.value = (numeroActual +1).toString().padStart(2, "0");}
-
-// });}
-
-// if(inputNumeroTurno !== undefined && inputNumeroTurno !== null && botonMenos !== null && botonMenos !== undefined){
-//     botonMenos.addEventListener("click", () => {
-//         if(inputNumeroTurno instanceof HTMLInputElement){
-//             let numeroActual = Number (inputNumeroTurno.value) || 0;
-//     inputNumeroTurno.value = (numeroActual - 1).toString().padStart(2, "0");}
-// });}
-
-// if(inputNumeroTurno !== null && inputNumeroTurno !== undefined && botonReset !== null && botonReset !== undefined){
-//     botonReset.addEventListener("click", () => {
-//         if(inputNumeroTurno instanceof HTMLInputElement){
-//             inputNumeroTurno.value = "00";}
-// });}
-
-// const nuevoTurnoInput = document.getElementById ("nuevoTurno");
-// const botonCambiarTurno = document.getElementById("botonCambiarTurno");
-// const numeroTurnoDisplay = document.getElementById("numeroTurno");
-
-// if (botonCambiarTurno !== null && botonCambiarTurno !== undefined && nuevoTurnoInput !== null && nuevoTurnoInput !== undefined) {
-//     botonCambiarTurno.addEventListener("click", () => {
-//         if (nuevoTurnoInput instanceof HTMLInputElement) {
-//             let nuevoTurno = nuevoTurnoInput.value;
-
-//             if (!isNaN(parseInt(nuevoTurno)) && numeroTurnoDisplay !== null && numeroTurnoDisplay !== undefined && numeroTurnoDisplay instanceof HTMLInputElement) {
-//                 numeroTurnoDisplay.value = nuevoTurno.padStart(2, "0");
-//             } else {
-//                 console.error("Introduzca un valor numérico");
-//             }
-//         }
-//     });
-// }
